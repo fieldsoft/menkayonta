526e3d4e864efee7e1c27da87e234452f49c4037
Noah Diewald <noah@diewald.me>
2025-09-28 18:58:52 -0400

    better platform specific window closing expectations

--- html/app.js
+++ html/app.js
@@ -3,21 +3,29 @@ const path = require('path')
 
 let isDev = process.env.APP_DEV ? process.env.APP_DEV.trim() == 'true' : false
 
-app.on('window-all-closed', () => app.quit())
-
 if (isDev) require("electron-reload")(__dirname, {
   electron: path.join(process.cwd(), 'node_modules', '.bin', 'electron')
 })
 
-app.whenReady().then(() => {
-  let mainWindow = new BrowserWindow({
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     frame: true,
     resizable: true,
   })
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.on('closed', () => {
-    mainWindow = null;
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
