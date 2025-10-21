3430a49d214cf3eb36950d74d5bd90200050f6e3
Noah Diewald <noah@diewald.me>
2025-10-21 14:44:42 -0400

    creating projects directory and global config on startup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'node:path';
+import fs from 'node:fs/promises';
+import os from 'node:os';
 import started from 'electron-squirrel-startup';
 
+// The pouchdb packages need commonjs imports
 const PouchDB = require('pouchdb-core')
 const HttpPouch = require('pouchdb-adapter-http')
 const mapreduce = require('pouchdb-mapreduce')
@@ -14,23 +17,28 @@ const sqliteAdapter = require('pouchdb-adapter-node-websql')
 // Register the adapter
 PouchDB.plugin(sqliteAdapter)
 
+
 // Create a database using the SQLite3 adapter
-const db = new PouchDB('mydb', { adapter: 'websql' })
+//const db = new PouchDB('mydb', { adapter: 'websql' })
 
 
-const doc = {
-  "_id": "mittens",
-  "name": "Mittens",
-  "occupation": "kitten",
-  "age": 3,
-  "hobbies": [
-    "playing with balls of yarn",
-    "chasing laser pointers",
-    "lookin' hella cute"
-  ]
-}
+// const doc = {
+//   "_id": "mittens",
+//   "name": "Mittens",
+//   "occupation": "kitten",
+//   "age": 3,
+//   "hobbies": [
+//     "playing with balls of yarn",
+//     "chasing laser pointers",
+//     "lookin' hella cute"
+//   ]
+// }
 
-db.put(doc)
+// db.put(doc)
+
+// Set some path variables
+const projectsPath = path.join(os.homedir(), 'Menkayonta')
+const globalConfPath = path.join(projectsPath, 'config')
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -67,18 +75,66 @@ const createWindow = () => {
       path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
   }
 }
+  
+const initialConf = { projects: [] }
 
-app.whenReady().then(() => {
-  ipcMain.on('set-title', handleSetTitle)
-  createWindow()
+// The openConf() function will create the projects directory and
+// global config file in the user's home directory if they do not
+// exist.
+const openGlobalConf = async () => {
+  let fh
 
-  // Open a window on MacOS when none are otherwise open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+  try {
+    await fs.mkdir(projectsPath, { recursive: true })
+    const confstats = await fs.state(globalConfPath)
+
+    fh = await fs.open(globalConfPath, 'r')
+    const configData = await fh.readFile({ encoding: 'utf8' })
+        
+    return JSON.stringify({ config: JSON.parse(configData) })
+  } catch (err) {
+    fh = await fs.open(globalConfPath, 'w+')
+    await fh.writeFile(JSON.stringify(initialConf))
+
+    return JSON.stringify({ config: initialConf })
+  } finally {
+    if (fh) {
+      await fh.close()
     }
-  })
-})
+  }
+}
+
+const init = async () => {
+  try {
+    await app.whenReady()
+
+    const globalConf = await openGlobalConf()
+  
+    ipcMain.on('set-title', handleSetTitle)
+    createWindow()
+
+    // Open a window on MacOS when none are otherwise open.
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// app.whenReady().then(() => {
+//   ipcMain.on('set-title', handleSetTitle)
+//   createWindow()
+
+//   // Open a window on MacOS when none are otherwise open.
+//   app.on('activate', () => {
+//     if (BrowserWindow.getAllWindows().length === 0) {
+//       createWindow()
+//     }
+//   })
+// })
 
 // On MacOS, don't close the application when all windows are closed
 app.on('window-all-closed', () => {
@@ -86,3 +142,5 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+init()
