5e8dd86537654406acfa589f7e6af0a89d3cf53b
Noah Diewald <noah@diewald.me>
2025-11-30 23:21:45 -0500

    static analysis tools

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(['.vite/**/*'], 'Ignore Build Directory'),
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+]);
