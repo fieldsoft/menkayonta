2d7c1ce8f7de416fdf014deaeee403d110a90ec2
Noah Diewald <noah@diewald.me>
2026-01-18 21:01:59 -0500

    fixing issues with tooling configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['.vite/**/*'], 'Ignore Build Directory'),
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node, 'MutationObserver'} } },
-]);
+  globalIgnores(['.vite/**/*', 'elm-stuff/**/*'], 'Ignore Build Directory'),
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+])
